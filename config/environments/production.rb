133874eab7e041183a7cc57c65c17f5e555b28cd
정필성 <ibetter.kr@gmail.com>
2026-04-19 15:09:58 +0900

    feat(part06/ch02): 02장. Solid Cache와 Solid Cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,1 @@
-Rails.application.configure do
-  # 구조화 로깅 활성화
-  config.log_formatter = ActiveSupport::Logger::Formatter.new
-
-  # JSON 형식으로 변경
-  config.logger = ActiveSupport::Logger.new($stdout)
-  config.log_level = :info
-
-  # Tagged Logging: 요청 ID를 모든 로그 줄에 포함
-  config.log_tags = [:request_id]
-end
+config.cache_store = :solid_cache_store
